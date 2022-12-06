001c1199bee63c2584c005f67b85c7717c3ab51c
mr. whalen <michael.whalen@sitechhs.com>
2022-12-06 09:21:15 -0500

    me py

---
+++ me.py
@@ -0,0 +1,7 @@
+me = {
+    "first_name":"Michael",
+    "last_name" : "Whalen",
+    "occupation" :"Teaching",
+    "interests": [{'name':"Video Games", "image":"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRvryjc1XPfXADvT7nPvNSnHYx7turyVMmwvA&usqp=CAU","desc":"Video Games are cool"}, {"name" : "Football", "image":"https://images.unsplash.com/photo-1566577739112-5180d4bf9390?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=1926&q=80", "desc":"I love fantasy football and all the stats surrounding it"}, {"name":"Programming", "image":"https://images.unsplash.com/photo-1587620962725-abab7fe55159?ixlib=rb-4.0.3&ixid=MnwxMjA3fDB8MHxzZWFyY2h8Mnx8cHJvZ3JhbW1pbmd8ZW58MHx8MHx8&auto=format&fit=crop&w=500&q=60", "desc":"I love programming and building web apps. There's always something new to learn"}],
+    "meme":"https://i.imgflip.com/6zu3w0.jpg"
+}
