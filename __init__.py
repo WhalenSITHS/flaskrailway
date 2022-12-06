1d4d8dd0cff68a740461bab3122390b6e3ac129b
mr. whalen <michael.whalen@sitechhs.com>
2022-12-06 09:32:58 -0500

    Revert "me py"
    This reverts commit 001c1199bee63c2584c005f67b85c7717c3ab51c.

--- __init__.py
+++ __init__.py
@@ -1,47 +1,13 @@
-from flask import Flask, render_template
+from flask import Flask, jsonify
 import os
 
-from .me import me
+app = Flask(__name__)
 
-def create_app(test_config=None):
-    # create and configure the app
-    app = Flask(__name__, instance_relative_config=True)
-    app.config.from_mapping(
-        SECRET_KEY='dev',
-        DATABASE=os.path.join(app.instance_path, 'flaskr.sqlite'),
-    )
 
-    if test_config is None:
-        # load the instance config, if it exists, when not testing
-        app.config.from_pyfile('config.py', silent=True)
-    else:
-        # load the test config if passed in
-        app.config.from_mapping(test_config)
+@app.route('/')
+def index():
+    return jsonify({"Choo Choo": "Welcome to your Flask app 🚅"})
 
-    # ensure the instance folder exists
-    try:
-        os.makedirs(app.instance_path)
-    except OSError:
-        pass
 
-  
-    @app.route("/")
-    def index():
-        return render_template('index.html', me=me)
-
-   
-    @app.route('/interest/<path:interest>')
-    def getInterest(interest):
-        def getInterest(interest, user):
-            try:
-                for element in user["interests"]:
-                    if interest in element['name']:
-                        return element
-            except:
-                return 'Not Found'
-            
-        
-        data = getInterest(interest, me)
-        return render_template('interest.html', data=data)
-    
-    return app
+if __name__ == '__main__':
+    app.run(debug=True, port=os.getenv("PORT", default=5000))
