410c66322312c18e03c14c75f39536cfaedc56a1
mr. whalen <michael.whalen@sitechhs.com>
2022-12-08 08:59:35 -0500

    create app

--- main.py
+++ main.py
@@ -51,7 +51,7 @@ def create_app(test_config=None):
         return render_template('interest.html', data=data)
     return app
     
-app = Flask(__name__)
+app = create_app()
 
 if __name__ == '__main__':
     app.run(debug=True, port=os.getenv("PORT", default=5000))
